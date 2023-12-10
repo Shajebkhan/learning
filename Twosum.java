ebc3c3c4f4f442cd702fbbf84b216125d5ba82e8
shajebkhan <shajebkhanbisalpur@gmail.com>
2023-12-10 12:13:33 +0530

    table of 3

--- Twosum.java
+++ Twosum.java
@@ -1,6 +1,6 @@
 public class Twosum {
     public static void main(String[] args) {
-        int input =2;
+        int input =3;
         for (int i = 1; i < 11; i++) {
             System.out.println(i*2);
         }
