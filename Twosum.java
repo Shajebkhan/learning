f245d347d5e343583e38cf4df87889b7cb26122c
shajebkhan <shajebkhanbisalpur@gmail.com>
2023-12-10 12:12:25 +0530

    creating file for 2 table

---
+++ Twosum.java
@@ -0,0 +1,8 @@
+public class Twosum {
+    public static void main(String[] args) {
+        int input =2;
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i*2);
+        }
+    }
+}
