b199659a149e891404a9d2625646abe9384927c4
shajebkhan <shajebkhanbisalpur@gmail.com>
2023-12-10 11:58:57 +0530

    use of if else condition

--- hi.java
+++ hi.java
@@ -1,5 +1,17 @@
 public class hi {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        int arr[] = {1,2,3,4};
+        for (int i = 0; i<arr.length; i++) {
+            if (arr[i]==3) {
+                System.out.println("your name is khan");
+            }
+                else{ 
+                    System.out.println("your name is not khan");
+                              
+                
+            }
+            
+        } 
+        // System.out.println("Hello word");
     }
 }
