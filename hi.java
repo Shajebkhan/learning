64cec1fc10461adcd0dbd47ac2a9fe84283471fd
shajebkhan <shajebkhanbisalpur@gmail.com>
2024-02-11 13:47:58 +0530

    makeing hello world

--- hi.java
+++ hi.java
@@ -1,17 +1,8 @@
+import java.util.Scanner;
+
 public class hi {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4};
-        for (int i = 0; i<arr.length; i++) {
-            if (arr[i]==3) {
-                System.out.println("your name is khan");
-            }
-                else{ 
-                    System.out.println("your name is not khan");
-                              
-                
-            }
-            
-        } 
-        // System.out.println("Hello word");
+        
+         System.out.println("Hello word");
     }
 }
