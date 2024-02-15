066be8fd782992241d5b8fa972701592774de2c5
shajebkhan <shajebkhanbisalpur@gmail.com>
2024-02-16 00:01:22 +0530

    find the target with the sum of pair

--- hi.java
+++ hi.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
 
 public class hi {
-    public static void main(String[] args) {
-        
-         System.out.println("Hello word");
+    public static int getlarest( int arr[]){
+    int n = arr.length;
+    for ( int i =0; i<n; i++){
+        boolean flag = true;
+        for (int j =0; j<n; j++ ) {
+            if (arr[j]> arr[i]){
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true);
+        return i;
     }
+    return -1;
+}
+public static void main(String[] args) {
+    int arr [] = {2,6,8,3,9};
+    System.out.println(getlarest(arr));
+}
 }
