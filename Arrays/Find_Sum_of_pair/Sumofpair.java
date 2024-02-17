7393de5c7e17fda104e38d84847eed47a15c0430
shajebkhan <shajebkhanbisalpur@gmail.com>
2024-02-17 21:38:26 +0530

    Adding sum of pair problem

--- Arrays/Find_Sum_of_pair/Sumofpair.java
+++ Arrays/Find_Sum_of_pair/Sumofpair.java
@@ -1,6 +1,8 @@
+package Arrays.Find_Sum_of_pair;
 public class Sumofpair {
 
     static int Sumofpair(int arr[], int x) {
+        
         int ans = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
